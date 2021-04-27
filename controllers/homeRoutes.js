6e30d6afadb613b0926130269fe6cf0e8038d4c4
Oliver Shih <olivershih@gmail.com>
2021-04-27 01:45:39 -0700

    finished posting, need to work on commenting

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,11 +42,25 @@ router.get("/post/:id", async (req,res) => {
     }
 });
 
+router.get("/dashboard", withAuth, async (req,res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ["password","email"]},
+            include: [{model:Post}]
+        });
+        const user = userData.get({plain: true});
+        console.log(user);
+        res.render("dashboard", {user,logged_in:req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.get("/login", (req,res) => {
-    // if (req.session.logged_in) {
-    //     res.redirect('/dashboard');
-    //     return;
-    // }
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
     res.render("login");
 });
 
---
+++ public/js/post.js
@@ -0,0 +1,23 @@
+const newPost = async event => {
+    event.preventDefault();
+    const title = document.getElementById("post-title").value.trim();
+    const text = document.getElementById("post-text").value.trim();
+    console.log(title);
+    console.log(text);
+    if (title && text) {
+        const response = await fetch("/api/post", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({title,text})
+        });
+
+        if (response.ok) {
+            alert("Post successful");
+            document.location.replace("/");
+        } else {
+            alert("Post failed");
+        }
+    }
+};
+
+document.getElementById("post-form").addEventListener("submit",newPost);
